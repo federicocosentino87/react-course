b09d91f11d6a3a1457080f94319d511920fa39a4
federico.cosentino <federico.cosentino@gmail.com>
2018-11-23 17:57:55 +0100

    season impl

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
     
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
 
+    //The same as initialize in constructor --> abbreviate syntax
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage : err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage : err.message })
         );
     }
 
+    componentDidUpdate() {
+        console.log('My component was just updated - rerendered');
+    }
+    
+    co
     render() {
         
         if (this.state.lat && !this.state.errorMessage) {
-           return  <div>Latitude: {this.state.lat}</div>;
+           return  <SeasonDisplay lat={this.state.lat}/>;
         }
 
         if (!this.state.lat && this.state.errorMessage) {
             return  <div>Error: {this.state.errorMessage}</div>;
         }
         
-        return  <div>Loading!</div>;
+        return  <Spinner />;
     }
 }
 
