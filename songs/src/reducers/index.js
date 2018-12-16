b106472fc31afff6a430b7c231e09ba7a39ad92a
Federico <cosentinofederico4@gmail.com>
2018-12-16 17:33:40 +0100

    first redux App

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -9,12 +9,12 @@ const songsReducer = () => {
     ];
 };
 
-const selectedSongReducer = (selecedSong=null, action) => {
+const selectedSongReducer = (selectedSong=null, action) => {
     if (action.type === 'SONG_SELECTED') {
         return action.payload;
     }
 
-    return selecedSong;
+    return selectedSong;
 };
 
 export default combineReducers({
